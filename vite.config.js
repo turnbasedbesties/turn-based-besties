f1785b564780ee3b7e4e8b413409d7a197d5f98d
drtootsie <peptz7@gmail.com>
2025-12-30 21:13:48 -0600

    fix: update vite base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // Set base to '/repo-name/' if hosted as a sub-project
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/turn-based-besties/', 
 })
